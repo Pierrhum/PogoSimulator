88f3b6e60b6dc3e55c9146c125abe98b04a23e50
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2023-01-20 10:21:38 +0100

    Fading screen improvements

--- Workshop Prog/Assets/Scripts/UI/FadingScreen.cs
+++ Workshop Prog/Assets/Scripts/UI/FadingScreen.cs
@@ -8,47 +8,8 @@ using UnityEngine.UI;
 
 public class FadingScreen : MonoBehaviour
 {
-    public UIShiny EnjmiaounShiny;
-    public Image Overlay;
-
-    private void Start()
+    private void LoadMainScene(string scene)
     {
-        StartCoroutine(ScreenCoroutine(1f, 1f));
-    }
-
-    private IEnumerator ScreenCoroutine(float ShinyDuration, float FadeDuration)
-    {
-        yield return new WaitForSeconds(1f);
-        // FadeIn
-        float timer = 0f;
-        while (timer < FadeDuration)
-        {
-            Overlay.color = new Color(Overlay.color.r, Overlay.color.g, Overlay.color.b,
-                Mathf.Lerp(1f, 0f, timer / FadeDuration));
-            timer += Time.deltaTime;
-            yield return new WaitForSeconds(Time.deltaTime);
-        }
-        
-        // Enjmiaoun Shiny
-        timer = 0f;
-        while (timer < ShinyDuration)
-        {
-            EnjmiaounShiny.effectFactor = timer / ShinyDuration;
-            timer += Time.deltaTime;
-            yield return new WaitForSeconds(Time.deltaTime);
-        }
-        
-        // FadeOut
-        timer = 0f;
-        while (timer < FadeDuration)
-        {
-            Overlay.color = new Color(Overlay.color.r, Overlay.color.g, Overlay.color.b,
-                Mathf.Lerp(0f, 1f, timer / FadeDuration));
-            timer += Time.deltaTime;
-            yield return new WaitForSeconds(Time.deltaTime);
-        }
-
-        yield return new WaitForSeconds(1f);
-        SceneManager.LoadScene("MainLevel");
+        SceneManager.LoadScene(scene);
     }
 }
